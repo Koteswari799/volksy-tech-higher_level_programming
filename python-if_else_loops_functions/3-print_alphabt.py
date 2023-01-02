5424f7a050f5c9327323f1eedcc7887dc50c810a
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-01 23:02:50 -0800

    3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')+1):
-    if i not in 'qe':
+    if i == ord('e') or ord('q') :
         continue
-        print("{:c}".format(i), end = "")
+    print("{:c}".format(i), end = "")

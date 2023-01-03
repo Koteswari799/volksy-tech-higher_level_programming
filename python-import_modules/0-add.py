09c82d1dbd0c12b56e77c950c21cd7808cce5b74
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-02 23:32:25 -0800

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
-    a = 1
-    b = 2
-    print('<a value> + <b value> = <add(a, b) value>')
-add(a,b)
+from 0_add import add
+if __name__ = "__main":
+    a=1
+    b=2
+    print('{:d} + {:d} = {;d} .format(a, b, add(a+b)))

008e3f30e574d1fef387f86f52469ec0dcffea0b
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-03 08:48:51 -0800

    1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if length <= 1:
         print("0 arguments.")
     elif length == 2:
-        print("{:d} argument".format(length - 1))
+        print("{:d} argument:".format(length - 1))
     else:
         print("{:d} arguments:".format(length - 1))
     for i in range(1,length):

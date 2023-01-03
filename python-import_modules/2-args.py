eacfdf6c2f0f110c36db6d4b2a87e99bdce30af8
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-03 08:54:38 -0800

    1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("{:d} argument:".format(length - 1))
     else:
         print("{:d} arguments:".format(length - 1))
-    for i in range(1,length):
+    for i in range(1, length):
         print("{:d}: {:s}".format(i, sys.argv[i]))

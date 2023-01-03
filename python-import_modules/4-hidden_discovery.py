37ee4955b54179f7f67534f1eab560f18fe4e9c1
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-03 10:06:36 -0800

    4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if _name_ == "__main__":
+if __name__ == "__main__":
     import hidden_4
     for i in dir(hidden_4):
-        if "_" not in i:
+        if "__" not in i:
             print(i)

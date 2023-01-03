57b950218f11d95e4896f66a894c9c4ff0c1851f
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-03 08:41:14 -0800

    1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    lenght = len(sys.argv)
-    if lenght <= 1:
+    length = len(sys.argv)
+    if length <= 1:
         print(" 0 arguments.")
-    elif lenght == 2:
+    elif length == 2:
         print("{:d} arguments".format(length - 1))
     else:
         print("{:d} arguments:".format(length - 1))
-    for i in range(1,lenght):
+    for i in range(1,length):
         print("{:d}: {:s}".format(i, sys.argv[i]))

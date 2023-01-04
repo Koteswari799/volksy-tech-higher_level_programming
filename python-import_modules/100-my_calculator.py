e7bdbae4dd5319228b16232d9c995169152e9e81
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-04 02:01:31 -0800

    7

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import agrv
+    if len(agrv) != 4:
+        print('./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    operator = agrv[2]
+    n1 = int(agrv[1])
+    n2 = int(agv[3])
+    if operator == '+':
+        print('{} + {} = {}'.format(n1, n2, add(n1, n2)))
+    elif operator == '-':
+        print('{} + {} = {}'.format(n1, n2, sub(n1, n2)))
+    elif  operator == '*':
+        print('{} + {} = {}'.format(n1, n2, mul(n1, n2)))
+    elif operator == '/':
+        print('{} + {} ={}'.format(n1. n2, div(n1, n2)))
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+
+

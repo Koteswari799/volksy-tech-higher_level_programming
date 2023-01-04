daf55cac28fcec6f7dd48c46cf97a1834b05e3e9
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-04 03:38:47 -0800

    8

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -4,7 +4,7 @@ def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
-            c = add(a,b)
+            c = add(a, b)
         return (c)
     else:
         return (sub(a, b))

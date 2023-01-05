884310f066e57bde4936804e27cf83c9146c3061
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-05 01:45:02 -0800

    10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list
-    for i in divisible_by_2(my_list=[]):
-        print('{:d}',format(i))
+    if my_list:
+        for i in divisible_by_2(my_list=[]):
+            print('{:d}',format(i))

daf8709c9949ddd0fbbf3e5a355cb2105d390b20
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-05 21:55:16 -0800

    6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    lst = []
-    for i in range(len(matrix)):
+    for i in len(matrix):
         for j in range(len(matrix[i])):
-            print('{:d}'.format(matrix[i][j],end='')
-            if j != (len(matrix[i]) - 1):
-                print(' '.end = ' ')
+            print('{:d}'.format(matrix[i][j].end = '')
+            if j != (len(matrix[i] - 1):
+                print(''.end='')
         print('')

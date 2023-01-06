c92e59200730ea337349e372ab1663529f18e66e
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-05 22:57:34 -0800

    i

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == 0:
+    if len(my_list) == 0:
         return None
-    else:
-        my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-        print("Max: {}".format(max(my_list))
-
-
+    max = my_list[0]
+    for i in my_list:
+        if i > max
+        max = i
+    return max

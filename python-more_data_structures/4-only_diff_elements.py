c41b3ab66f64572fff2b41619650db09643079dc
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-06 03:08:10 -0800

    4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -4,7 +4,7 @@ def only_diff_elements(set_1, set_2):
     for i in set_1:
         if i not in set_2:
             lst.append(i)
-    for i not in set_2:
+    for i in set_2:
         if i not in set_1:
             lst.append(i)
     return lst

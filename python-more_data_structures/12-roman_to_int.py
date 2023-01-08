6d44c25de7951c333f3b92af4c5f8a83b6b73bbb
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-08 08:12:21 -0800

    12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    def best_score(a_dictionary):
-    if a_dictionary:
-        max_value = list(a_dictionary.keys())[0]
-        for x in a_dictionary.keys():
-            if a_dictionary[x] > a_dictionary[max_value]:
-                max_value = x
-        return max_value
-    else:
-        return None
+    rom_num = {'I': 1,
+               'V': 5,
+               'X': 10,
+               'L': 50,
+               'C': 100,
+               'D': 500,
+               'M': 1000
+               }
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    converted = 0
+    length = len(roman_string)
+    for i in (length): 
+        if i is (length -1 ):
+            converted += rom_num[roman_string[i])
+        else:
+            if rom_num[roman_string[i]] >= rom_num[roman_string[i + 1]]:
+                converted += rom_num[roman_string[i]]
+            else:
+                converted -= rom_num[roman_string[i]]
+    return (converted)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      

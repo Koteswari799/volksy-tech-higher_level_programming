1731fd397c0c3a48e0c802787707097944880e09
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-08 05:19:24 -0800

    6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    li = my_list[:]
-    for i in range(len(li)):
-        if li[i] == search:
-            li[i] = replace
-    return li
+def print_sorted_dictionary(a_dictionary):
+    j = 0
+    for i in sorted(a_dictionary):
+        print("{}: {}".format(i, a_dictionary[i]))

15eb3877dfd5ba750e720b9e252ad7df8e1b5843
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-09 10:08:00 -0800

    4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -10,7 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print('division by 0')
         except IndexError:
             print('out of range')
-            result = =0
+            result = 0
         finally:
             return_list.append(result)
     return return_list

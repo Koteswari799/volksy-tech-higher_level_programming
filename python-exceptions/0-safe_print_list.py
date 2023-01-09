f8b8abe26bb0e301aeafb32c11f9440546b2be46
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-09 02:39:50 -0800

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(0, x):
         try:
             print('{:d}'.format(my_list[i]), end ="")
-            count += count 
+            count += 1
         except:
             pass
     print('')

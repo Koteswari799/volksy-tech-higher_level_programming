aed9d49fb18d302dab049533da967b3d5cbac84c
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-09 02:27:31 -0800

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0:
-        for i in range(0, x):
-            try:
-                print('{:d}'.format([i]), end ="")
-                count = count + 1
-            except:
-                print('index error')
+    count = 0
+    for i in range(0, x):
+        try:
+            print('{:d}'.format([i]), end ="")
+            count = count + 1
+        except:
+            pass
+        print('')
+        return count

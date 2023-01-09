16a0c41b27a23594459de52e94109dd5f0001860
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-09 10:42:31 -0800

    7

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
     try:
-        print('{:d}.format(value))
+        print('{:d}'.format(value))
         return True
     except ValueError as err:
     except Exception as err:

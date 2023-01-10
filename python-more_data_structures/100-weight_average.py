93e189e397ea8d7661211b51e80c91b9f394c08d
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-09 21:12:44 -0800

    13

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0 or not isinstance:
-        return 0
+    if len(my_list) == 0 or not isinstance(my_list,list):
+        return (0)
     avg = 0
     size = 0
     for tup in my_list:

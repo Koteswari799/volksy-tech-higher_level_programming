c823a96457ece3b0ec1285f849b88c4dcd555ada
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-11 03:49:09 -0800

    3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,9 +7,9 @@ class Square:
     def __init__(self, size=0):
         '''constuctor'''
         if type(size) is not int:
-            return TypeError('size must be an integer')
+            raise TypeError('size must be an integer')
         if size < 0:
-            return ValueError('size must be >= 0')
+            raise ValueError('size must be >= 0')
         self.__size = size
 
     def are(self):

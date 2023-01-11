b5e35a3f5df2d023dcf1a7a307510728a9bd2db0
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-11 04:09:01 -0800

    4

---
+++ python-classes/4-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''square'''
+class Square:
+    '''square size'''
+    def __init__(self, size=0):
+        self.size = size
+
+    def size(self):
+        return self,__size
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self,__size = value
+    def area(self):
+        return self,__size * self,__size

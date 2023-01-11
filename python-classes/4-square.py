eeb079567bfa4f7597abc8dc9799e22973bed48b
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-11 07:31:12 -0800

    4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""string"""
+'''square'''
 
 
 class Square:
-    """string size"""
+    '''square size'''
+    def __init__(self, size=0):
+        self.size = size
 
-   def __init__(self, size=0):
-       self.size = size
+     @property
+     def size(self):
+         return self.__size
 
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+     @size,setter
+     def size(self, value):
+         if type(value) not in int:
+             raise TypeError('size must be an integer')
+       if value < 0:
+           raise ValueError('size must be >= 0')
+       self.__size = value
 
     def area(self):
         return self.__size * self.__size
+

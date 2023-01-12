a69e051d93dba9679716211facaa3c40fe1c8681
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-11 19:57:44 -0800

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,6 +8,10 @@ class Square:
     def _init_(self, size=0):
         self.size = size
 
+    def area(self):
+        """area of the square"""
+        return(self.__size * self.__size)
+
     @property
     def size(self):
         return self.__size
@@ -21,8 +25,3 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area():
-        """area of square"""
-
-        return (self._size * self._size)

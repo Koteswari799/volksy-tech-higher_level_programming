a2347991299a24a1619dba6d931d1dcba030789e
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-11 22:37:10 -0800

    6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -13,7 +13,7 @@ class Square:
     def size(self):
         return self.__size
 
-    @ size.setter
+    @size.setter
     def size(self, value):
         if type(value) != int:
             raise TypeError('size must be an integer')
@@ -25,6 +25,7 @@ class Square:
     def position(self):
         return self.__position
 
+
     @position.setter
     def position(self, value):
         if type(value) != tuple or len(value) != 2:
@@ -33,7 +34,7 @@ class Square:
             raise TypeError('position must be a tuple of 2 positive integers')
         if value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
-        self.__position = value
+    self.__position = value
 
     def area(self):
         a = self.__size * self.__size
@@ -45,9 +46,9 @@ class Square:
         else:
             for line in range(0, self.__position[1]):
                 print()
-            for i in range(0, self.__size):
-                for space in range(0, self.__position[0]):
-                    print('', end='')
-                for j in range(0, self.__size):
-                    print('#', end='')
-                print()
+                for i in range(0, self.__size):
+                    for space in range(0, self.__position[0]):
+                        print('', end='')
+                        for j in range(0, self.__size):
+                            print('#', end='')
+                        print()

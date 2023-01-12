ea183041eeb67b1f107aa4dba3b78ae15dca685e
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-11 20:42:24 -0800

    5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-'''square'''
-class Square:
-    '''square size'''
+"""class"""
 
+
+class Square:
+    """Access and update private attribute"""
 
     def __init__(self, size=0):
         self.size = size
+
+    def area(self):
+        """area of the square"""
+        return(self.__size * self.__size)
 
     @property
     def size(self):
@@ -13,20 +18,22 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """constructor"""
+
         if type(value) != int:
-            raise TypeError('size must be an integer')
-        elif value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size =value
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         a = self.__size * self.__size
         return a
 
     def my_print(self):
-        if self.__size = 0:
+        if self.__size == 0:
             print('')
-        for i in range(0, self. __size):
+        for i in range(0, self.__size):
             for j in range(0, self.__size):
                 print('#', end='')
             print()

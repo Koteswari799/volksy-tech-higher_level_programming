f1085cb82730e0e46bfaea0f9c5b81276265f65b
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-13 01:30:56 -0800

    3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''list'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''size of a class'''
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False

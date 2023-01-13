d0d33b2a9bbb6bef20cd1befca3d1ccee930be8c
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-13 01:56:24 -0800

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Contains a function area()
+    """
+
+    def area(self):
+        """Function not implemented yet
+        """
+
+        raise Exception("area() is not implemented")
+

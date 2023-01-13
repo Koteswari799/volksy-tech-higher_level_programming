ecd6f538784bfcce74b084ba230df798930db475
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-13 01:45:12 -0800

    5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -4,11 +4,4 @@
 
 
 class BaseGeometry:
-    """Contains a function area()
-    """
-
-    def area(self):
-        """Function not implemented yet
-        """
-
-        raise Exception("area() is not implemented")
+    pass

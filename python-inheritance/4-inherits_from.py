32737a7ab07e15c2fd9e12e3ff342c20d592c890
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-13 01:39:39 -0800

    5

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-'''list'''
+"""inherits_from
+"""
 
 
 def inherits_from(obj, a_class):
-    '''size of list'''
+    """Function that returns True if the object is an instance
+    of a class that inherited (directly or indirectly) from the
+    specified class; otherwise False.
+    """
 
     if type(obj) is a_class or not isinstance(obj, a_class):
-        return True
-    else:
         return False
+    else:
+        return True
+

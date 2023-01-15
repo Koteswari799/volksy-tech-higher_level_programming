55d780db033cfa24e15783fba9ada4d46fe94cd4
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-15 06:54:28 -0800

    9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,28 +1,72 @@
 #!/usr/bin/python3
-"""Rectangle inherits from BaseGeometry"""
+"""
+This is the "Rectangle"  module.
+This module provides a Rectangle class.
+"""
 
 
-BaseGeometry = _import_('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle data inherits from BaseGeometry
+class Rectangle:
+    """A Rectangle class with attributes width and height,
+    methods area, perimeter, print, str, repr, and del, and
+    class attribute number_of_instances that keeps track of # of instances,
+    and class attribute print_symbol which is used as symbol for printing.
     """
 
-    def _init_(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __str__(self):
+        total = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                try:
+                    total += str(self.print_symbol)
+                except Exception:
+                    total += type(self).print_symbol
+            if i is not self.__height - 1:
+                total += "\n"
+        return total
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     def area(self):
-        """Method that calculates area of rectangle
-        """
+        return self.__width * self.__height
 
-        return self._width * self._height
-
-    def _str_(self):
-        """Magic method to return rectangle description
-        """
-
-        return "[Rectangle] {}/{}".format(self._width, self._height)
+    def perimeter(self):
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return (2 * self.__width) + (2 * self.__height)

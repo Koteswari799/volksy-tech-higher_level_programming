f0f13a09ddadd6cfa0cda6e8e809bcce4019c271
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-17 20:11:06 -0800

    11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,50 +1,18 @@
 #!/usr/bin/python3
-""" 11-square """
-
-
-class BaseGeometry:
-    """ Class BaseGeometry """
-    def area(self):
-        """ raises an exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ validates value """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """ Class Rectangle subclass of BaseGeometry"""
-    def __init__(self, width, height):
-        """ class constructor """
-        self.__width = width
-        self.__height = height
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ Class Rectangle subclass of Rectangle subclass of BaseGeometry"""
+    """Represent a square."""
+
     def __init__(self, size):
-        """subclass constructor"""
-        self.__size = size
-        BaseGeometry.integer_validator(self, "size", size)
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
         super().__init__(size, size)
-
-    def area(self):
-        """ returns area of square"""
-        return self.__size * self.__size
-
-    def __str__(self):
-        return ("[Square] {}/{}".format(self.__size, self.__size))
-Footer
-
+        self.__size = size

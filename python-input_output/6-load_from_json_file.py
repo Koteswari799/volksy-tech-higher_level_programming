77fc258cfe0f0492dbb9d68c513c2c14f992e54d
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-18 03:58:27 -0800

    6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''write a file'''
+import json
+
+
+def load_from_json_file(filename):
+    '''file name'''
+    with open(filename,mode='r', encoding='UTF-8') as f:
+        return json.load(filename)

16c1311a061c740aef290df805c99c82fc615c29
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-18 02:45:51 -0800

    2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,8 +2,9 @@
 '''string'''
 
 
+
 def append_write(filename="", text=""):
-    '''file name'''
+    '''write'''
     if filename:
         with open(filename, mode='a', encoding='UTF-8') as f:
             return (f.write(text)):

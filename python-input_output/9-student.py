c6cb192b710e5264be0f7c6909ff5233c9f2a563
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-18 20:47:59 -0800

    9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''write a file'''
+class Student:
+    '''create a class'''
+
+
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''create a function'''
+
+        return self.__dict__

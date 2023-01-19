3699db971dc4e6e1ae4357c6111ca8366644a828
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-18 21:22:09 -0800

    10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-'''write a file'''
-class Student:
-    '''create a class'''
+"""Defines a class student."""
 
+
+class Student:
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        '''create a file'''
-        self.
+        """Initailize a new Student."""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation  of the Student."""
+
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__

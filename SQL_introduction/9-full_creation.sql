2011678a3d24d4d9e86a0fe98c25b60b2203f29f
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-30 00:38:39 -0800

    9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
 -- hii
 CREATE TABLE IF NOT EXISTS second_table(id INT,name VARCHAR(256),score INT);
-INSERT INTO IF not exists second_table(id,name,score) values((1,'John ',10),(2,'Alex',3),(3,'Bob',14),(4,'George',8));
+INSERT INTO IF not exists second_table(id,name,score) values((1,'John',10),(2,'Alex',3),(3,'Bob',14),(4,'George',8));
 

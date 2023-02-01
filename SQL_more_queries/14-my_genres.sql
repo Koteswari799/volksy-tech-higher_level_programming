8512d96d5b0a6bae1ba3167a8009d138b9b0757c
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-02-01 03:42:56 -0800

    14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- hii
-SELECT tv_genres.name as name
+SELECT tv_genres.name AS name
 FROM tv_genres INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres genre_id
 FROM tv_show_genres INNER JOIN tv_shows

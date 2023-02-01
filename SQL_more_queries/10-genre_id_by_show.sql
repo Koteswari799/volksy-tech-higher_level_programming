cf36aff809443f5f8512e6fe20dd15cc033f35a8
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-31 22:50:42 -0800

    10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title,tv_show_genres.genre_id FROM tv_shows
  INNER JOIN tv_shows_genres
  ON  tv_show_genres.show_id = tv_shows.id
- ORDER BY tv_shows.title AND tv_show_genres.genre_id;
+ ORDER BY tv_shows.title,tv_show_genres.genre_id ASC;

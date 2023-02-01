c60a1085ee654b80e05be2af659872db94492013
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-02-01 09:46:25 -0800

    14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
 -- hii
-SELECT tv_genres.name AS name
+SELECT tv_genres.name
 FROM tv_genres INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-FROM tv_show_genres INNER JOIN tv_shows
+INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 WHERE title = 'Dexter'
-ORDER BY name ASC;
+ORDER BY tv_genres.name;

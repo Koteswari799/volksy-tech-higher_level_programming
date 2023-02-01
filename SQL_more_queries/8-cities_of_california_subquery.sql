c9dce588284d204e2a838eb738de3b2f009067b7
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-31 21:11:31 -0800

    8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,3 @@
 -- hii
-SELECT id,name FROM cities WHERE state_id = (SELECT id FROM STATES WHERE name = 'California') ORDER BY ID ASC;
+SELECT id,name FROM cities WHERE state_id = (SELECT id FROM STATES WHERE name = 'California') 
+ORDER BY id ASC;

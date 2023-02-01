cc7e36b8802d1fa5d33e888fbbd433808c37b799
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-01-31 22:11:30 -0800

    9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- hii
-SELECT cities.id,cities.name,states.name FROM ststes INNER JOIN cities ON states.id = cities.state_id WHERE ORDER BY id ASC;
+SELECT cities.id,cities.name,states.name FROM ststes INNER JOIN cities ON states.id = cities.state_id WHERE ORDER BY cities.id ASC;

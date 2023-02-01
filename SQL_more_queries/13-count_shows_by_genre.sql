853a97aaa75f067974f98eeb7959c853186167db
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-02-01 03:20:24 -0800

    13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_show_genres INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY genre
-ORDER BY genre
+ORDER BY genre DESC;

ad9284778d58281c23682513b01f0ba9b0fadb12
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-02-07 06:23:41 -0800

    181
    git commit -m  181

---
+++ python-classes/100-singly_linked_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''node'''
+
+
+class Node:
+    '''node size'''
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__size
+
+    @size.setter
+    def data(self, value):
+        if type(value) != int:
+            raise TypeError('data must be an integer')
+        self.__data = value
+
+
+

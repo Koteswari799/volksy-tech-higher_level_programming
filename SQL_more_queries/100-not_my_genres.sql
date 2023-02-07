8682b2e6e59b5f57e02728d5e21cb34c5929e823
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-02-07 05:48:52 -0800

    17

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,7 @@
+-- hii
+SELECT tv_genres.name
+FROM tv_genres INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE title NOT IN Dexter;

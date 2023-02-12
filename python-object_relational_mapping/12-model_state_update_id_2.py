0277e1efae7bde644a47feef1340a379bfb6fe17
Koteshwari799 <lakshmid@brightcomgroup.com>
2023-02-12 10:08:31 -0800

    12

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,11 +3,6 @@
    object from the database hbtn_0e_6_usa
 """
 from model_state import Base, State
-#!/usr/bin/python3
-"""script that changes the name of a State
-   object from the database hbtn_0e_6_usa
-"""
-from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 from sqlalchemy import create_engine

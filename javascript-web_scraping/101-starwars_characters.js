7c682958440d227a618c06ae5f2b4ee9b014f26c
root <lakshmid@brightcomgroup>
2023-02-28 20:39:39 -0800

    0

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,6 +1,29 @@
 #!/usr/bin/node
-// using Star Wars API, prints all characters of a given film
+// using Star Wars API, prints all `characters` of a given film, in order
 const request = require('request');
+
+function getCharName (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(body).name);
+    });
+  });
+}
+
+async function charsInFilm (urlList) {
+  try {
+    let name;
+    for (const url of urlList) {
+      name = await getCharName(url);
+      console.log(name);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 const filmsURL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(filmsURL, function (error, response, body) {
@@ -8,12 +31,5 @@ request(filmsURL, function (error, response, body) {
     console.error(error);
   }
   const urlList = JSON.parse(body).characters;
-  for (const url of urlList) {
-    request(url, function (error, response, body) {
-      if (error) {
-        console.error(error);
-      }
-      console.log(JSON.parse(body).name);
-    });
-  }
+  charsInFilm(urlList);
 });
